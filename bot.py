610a6d3b6477bd11a08e49807823420c072f9c78
Hemanth B <hemanth22hemu@gmail.com>
2022-05-28 14:04:17 +0530

    feat: adding telegram notification

---
+++ bot.py
@@ -0,0 +1,5 @@
+import telepot
+token = os.environ.get("TELE_CHANNEL")
+receiver_id = os.environ.get("CHATTER_ID") # https://api.telegram.org/bot<TOKEN>/getUpdates
+bot = telepot.Bot(token)
+bot.sendMessage(receiver_id, 'keepalive_jfrog: Job Completed Notification.') # send a activation message to telegram receiver id
